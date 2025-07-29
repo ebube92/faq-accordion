3a6bb96a06b3dd97e69e4c63e8c58036e708bdc6
Ebube Okafor <ebubeokafor02@gmail.com>
2025-07-30 00:11:43 +0100

    implemented logic to open and close questions

---
+++ script.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll('button[id^="question-"]');
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const answerId = btn.id.replace("question", "answer");
+    const answer = document.getElementById(answerId).querySelector("p");
+    const isClosed = answer.classList.contains("hidden");
+
+    const iconMinus = btn.querySelectorAll("#icon-minus");
+    const iconPlus = btn.querySelectorAll("#icon-plus");
+
+    console.log("Button clicked:", btn.id);
+    console.log("Answer ID:", answerId);
+    console.log("Is answer closed?", isClosed);
+
+    if (isClosed) {
+      answer.classList.remove("hidden");
+      answer.classList.add("block");
+      answer.classList.replace("opacity-0", "opacity-100", 600);
+      console.log("Answer is now visible");
+      iconMinus.forEach((icon) => icon.classList.remove("hidden"));
+      iconPlus.forEach((icon) => icon.classList.add("hidden"));
+    } else {
+      answer.classList.add("hidden");
+      answer.classList.remove("block");
+      answer.classList.replace("opacity-100", "opacity-0", 600);
+      console.log("Answer is now hidden");
+      iconMinus.forEach((icon) => icon.classList.add("hidden"));
+      iconPlus.forEach((icon) => icon.classList.remove("hidden"));
+    }
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "work-sans": ['"Work Sans"', "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
